89ec64b11d92cebbcded862b1728b577eb210fd4
NP3coder <79170757+NP3coder@users.noreply.github.com>
2023-03-17 15:59:56 -0500

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,18 @@
+function adduser() {
+    user_name = document.getElementById9("user_name").value;
+    localStorage.setItem = ("user_name", user_name);
+
+    window.location = "kwitter_room.html";
+
+}
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYED8JHSNGQlK0oj9KZSI2RluaGyjYfhY",
+  authDomain: "kwitter-b1c4e.firebaseapp.com",
+  databaseURL: "https://kwitter-b1c4e-default-rtdb.firebaseio.com",
+  projectId: "kwitter-b1c4e",
+  storageBucket: "kwitter-b1c4e.appspot.com",
+  messagingSenderId: "101262757432",
+  appId: "1:101262757432:web:e138b98fb6689f1a4460cd",
+  measurementId: "G-HVTZRXSRTB"
+};
