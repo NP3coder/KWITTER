5dbe1e4a5744099c4bb9582408c4413b308491dd
NP3coder <79170757+NP3coder@users.noreply.github.com>
2023-03-17 19:33:59 -0500

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,10 +1,8 @@
 
-//ADD YOUR FIREBASE LINKS HERE
+https://kwitter-b1c4e-default-rtdb.firebaseio.com/
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
-      //Start code
-
-      //End code
+ 
       });});}
 getData();
